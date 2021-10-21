a480287c2e5b576ecd392cef4c149ec6f84af1c4
maxkohn <kohn.max@gmail.com>
2021-10-21 10:05:35 -0600

    updated js pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//grab button value
+//click button action
+//create html to show question and answers as clickable actions
+//when user clicks an answer, check if correct answer
+    //if correct answer show correct and move to next question
+    //if wrong, show wrong and let user try again
+
+
+    //repeat for 3 questions
+    //when user gets 3 correct answers
+    //store timer value as high score
