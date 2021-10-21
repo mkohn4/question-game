bb967161ff27d1616f3940405b2f8e473532fa55
maxkohn <kohn.max@gmail.com>
2021-10-21 13:04:38 -0600

    add timer click action

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,32 @@
     //repeat for 3 questions
     //when user gets 3 correct answers
     //store timer value as high score
+
+
+var startGame = document.getElementById("start-game");
+var timerEl = document.getElementById('timer');
+
+function countdown() {
+    //set time left in timer
+    var timeLeft = 60;
+    //run function once a second
+    var timeInterval = setInterval(function() {
+      
+      if (timeLeft >= 1 ) {
+          //run time left every second
+          timerEl.textContent = timeLeft;
+          //decrement timeLeft
+          timeLeft--;
+      } else {
+        timerEl.textContent = 'Time Up!';
+        clearInterval(timeInterval);
+      }
+      
+  
+    }, 1000);
+  };
+
+
+
+//onclick of each container
+startGame.addEventListener("click", countdown() );
